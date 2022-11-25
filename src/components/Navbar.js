aa1883c75b1a4ac01e204caf1404bc8b193b7210
Vinicius Maciel <80479473+macielvini@users.noreply.github.com>
2022-11-25 20:18:26 -0300

    fix: updated sidebar initial status -> false

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
-  const [sidebar, setSidebar] = useState(true);
+  const [sidebar, setSidebar] = useState(false);
 
   const showSidebar = () => setSidebar(!sidebar);
 
