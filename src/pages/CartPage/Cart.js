97bf8d9ebf4e85136586621557f636b76d4c8a01
Lucas <lucass.ambar@gmail.com>
2022-11-25 16:32:02 -0300

    feat: cep verification

--- src/pages/CartPage/Cart.js
+++ src/pages/CartPage/Cart.js
@@ -1,29 +1,82 @@
 import styled from "styled-components";
 import CartProduct from "../../components/CartProduct";
 
+import back from "../../assets/images/back.png"
+import { useNavigate } from "react-router-dom";
+import Infos from "./infos";
+
 export default function Cart() {
+  const navigate = useNavigate()
+
   return (
     <Background>
-      <Container>
-        <CartProduct/>
-      </Container>
-    </Background>
+        <Img><img src={back} alt="back icon" onClick={() => navigate("/")}/></Img>
+        <Titulo>Meu carrinho</Titulo>
+
+        <ContainerProducts>
+          <CartProduct/>
+          <CartProduct/>
+          <CartProduct/>
+        </ContainerProducts>
+
+        <Subtitulo>Informações do pedido</Subtitulo>
+
+        <Infos/>  
+     </Background>
   );
 }
 
 const Background = styled.div`
-  width: 100vw;
   height: 100vh;
-  background-color: #FDFBF0;
+  background-color: #FAFCFC;
 
   display: flex;
-  justify-content: center;
-  padding-top: 50px;
+  flex-direction: column;
+  align-items: center;
 `
 
-const Container = styled.div`
-  height: 650px;
-  width: 300px;
-  border-radius: 15px;
-  background-color: #FFFFFF;
+const ContainerProducts = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`
+
+const Img = styled.div`
+  width: 255px;
+  height: 70px;
+  
+  img{
+    height: 20px;
+    width: 20px;
+    margin-top: 50px;
+
+    cursor: pointer;
+  }
+`
+const Titulo = styled.p`
+  font-family: Montserrat;
+  font-size: 20px;
+  font-weight: 600;
+  line-height: 24px;
+  letter-spacing: 0em;
+  text-align: left;
+  color:#000000;
+
+  display: block;
+  width: 255px;
+  margin-top: 15px;
+  margin-bottom: 15px;
+`
+const Subtitulo = styled.p`
+  font-family: Montserrat;
+  font-size: 20px;
+  font-weight: 500;
+  line-height: 24px;
+  letter-spacing: 0em;
+  text-align: left;
+  color: #000000;
+
+  display: block;
+  width: 255px;
+  margin-top: 25px;
 `
